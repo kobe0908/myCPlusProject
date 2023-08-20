40beeed004ac5bfa51092fc6813336e4d939717a
Jeremy Kuna <1620390019@qq.com>
2023-08-20 11:15:52 +0800

    add file

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    int num1, num2, sum;
+
+    std::cout << "请输入第一个整数：";
+    std::cin >> num1;
+
+    std::cout << "请输入第二个整数：";
+    std::cin >> num2;
+
+    sum = num1 + num2;
+
+    std::cout << "两个数的和为：" << sum << std::endl;
+
+    return 0;
+}
